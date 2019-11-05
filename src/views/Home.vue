2668021f3e236f7105415b9730aa7853ade6a14f
Erhilds <erickh.andreas@gmail.com>
2019-11-06 01:39:50 +0700

    Implementing axios for getting data and dispalying content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,48 @@
 <template>
-  <div class="home">
+  <div class="container py-2">
+    <div
+      v-for="(film, i) in films"
+      :key="`films__${i}`"
+      class="flex flex-wrap justify-center"
+    >
+      <app-card-film
+        :film-title="film.title"
+        :film-description="film.description"
+        :film-director="film.director"
+        :film-producer="film.producer"
+        :release-year="film.release_date"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import AppCardFilm from '@/components/AppCardFilm'
 
 export default {
   name: 'home',
   components: {
+    AppCardFilm
+  },
+  data () {
+    return {
+      films: [],
+      objects: 6
+    }
+  },
+  mounted () {},
+  created () {
+    this.getDataFilms()
+  },
+  methods: {
+    async getDataFilms () {
+      try {
+        const { data } = await this.axios.get('/films')
+        this.films = data
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
 }
 </script>
