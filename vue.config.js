e1b0854c702a69a570a3100e5d114240b090a5f2
Erhilds <erickh.andreas@gmail.com>
2019-11-06 03:43:16 +0700

    Edit vue config file

--- vue.config.js
+++ vue.config.js
@@ -4,14 +4,14 @@ module.exports = {
     themeColor: '#4DBA87',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
+    appleMobileWebAppStatusBarStyle: 'black'
 
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js'
-      // ...other Workbox options...
-    }
+    // // configure the workbox plugin
+    // workboxPluginMode: 'InjectManifest',
+    // workboxOptions: {
+    //   // swSrc is required in InjectManifest mode.
+    //   swSrc: 'dev/sw.js'
+    //   // ...other Workbox options...
+    // }
   }
 }
