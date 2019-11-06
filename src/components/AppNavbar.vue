e9759df2bca1b2669537a7708be52cd0b4a8510a
Erhilds <erickh.andreas@gmail.com>
2019-11-07 00:07:11 +0700

    Add button accessible name

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -5,7 +5,7 @@
         <img class="h-10" src="/img/ghibli-text.png" alt="Workcation">
       </div>
       <div class="sm:hidden">
-        <button aria-label="menu-burger" @click="isOpen = !isOpen" type="button" class="block text-gray-500 focus:text-white focus:outline-none">
+        <button id="menu-burger" aria-label="menu-burger" @click="isOpen = !isOpen" type="button" class="block text-gray-500 focus:text-white focus:outline-none">
           <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
             <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
             <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
